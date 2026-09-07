377a615c00a590078b83b1a5e7b738ca54c1b975
academictechinc <joe@academictechinc.com>
2026-09-07 00:36:33 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'weather-app-v12';
+const CACHE_NAME = 'weather-app-v14';
 const SHELL_ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
